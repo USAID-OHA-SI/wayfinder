d324230e974af0ea58c424687271c6252cdc472c
Aaron Chafetz <achafetz@usaid.gov>
2023-08-01 17:22:30 -0400

    modify funciton to handle diff structure and to move images in sub directory

--- utilities.R
+++ utilities.R
@@ -6,29 +6,46 @@ library(quarto)
 
 
 render_move <- function(qmd_file, 
-                        folderpath_input = "sections",
                         folderpath_output = "../usaid-oha-si.github.io/reference"){
   
-  #create a folder in on the blog repo
-  path_out <- file.path(folderpath_output, sub("\\.qmd", "", basename(qmd_file)))
+  #section
+  section <- gsub("\\.qmd", "", basename(qmd_file))
+  
+  #create a folder path in on the blog repo (delete if it already exists)
+  path_out <- file.path(folderpath_output, section)
+  if(dir.exists(path_out))
+    dir_delete(path_out)
   dir_create(path_out)
   
   #render qmd
   quarto_render(qmd_file)
   
   #identify files to move (non-qmd)
-  output_files <- list.files(folderpath_input, full.names = TRUE) 
+  output_files <- list.files(pattern = section, full.names = TRUE) 
   output_files <- output_files[str_detect(output_files, 'qmd$', negate = TRUE)]
   
   #move files to blog repo
   file_move(output_files, path_out)
+  
+  #create a folder path in on the blog repo for images
+  path_img_out <- file.path(path_out, "images")
+  
+  #move related images
+  dir_create(path_img_out)
+  
+  #identify images
+  img <- list.files("images", section, full.names = TRUE)
+  
+  #copy files
+  file_copy(img, path_img_out)
+  
 }
 
 
 # RENDER + MOVE FILES TO BLOG ---------------------------------------------
 
   #identify qmd files to render and move
-  files_input <- list.files("sections", "qmd", full.names = TRUE)
+  files_input <- list.files(pattern = "qmd", full.names = TRUE)
   
   #render + move
   walk(files_input, render_move)
@@ -41,7 +58,7 @@ render_move <- function(qmd_file,
   img_dir_out <- dir_create("../usaid-oha-si.github.io/assets/img/manuals/")      
   
   #identify images to move
-  files_img <- list.files("assets/img/manuals/", full.names = TRUE)
+  files_img <- list.files("images", full.names = TRUE)
   
   #move files
   walk(files_img, ~file_copy(.x, img_dir_out))
